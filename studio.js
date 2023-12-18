607bf01971831d5756219ff2b8eab6a0cab1e066
Eunryeong Kim <ker26772@gmail.com>
2023-12-18 22:29:18 +0900

    단위평가

---
+++ studio.js
@@ -0,0 +1,26 @@
+// 이미지 클릭 시 모달 창 열기
+function openModal(imagePath) {
+  const modal = document.getElementById('myModal');
+  const modalImg = document.getElementById('modalImg');
+
+  // 모달 창 열기
+  modal.style.display = 'block';
+  modalImg.src = imagePath;
+}
+
+// 모달 창 닫기
+function closeModal() {
+  const modal = document.getElementById('myModal');
+
+  // 모달 창 닫기
+  modal.style.display = 'none';
+}
+
+// 모달 바깥 부분 클릭 시 모달 창 닫기
+window.onclick = function (event) {
+  const modal = document.getElementById('myModal');
+  
+  if (event.target === modal) {
+    closeModal();
+  }
+};
